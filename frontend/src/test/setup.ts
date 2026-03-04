24b77bb8fef6550adbf0934fe5e3827763e156bf
wangjohn <john.jian.wang@gmail.com>
2026-03-03 20:46:14 -0800

    Improve team settings actions UI

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -10,6 +10,22 @@ globalThis.ResizeObserver ??= class ResizeObserver {
   disconnect() {}
 } as unknown as typeof globalThis.ResizeObserver;
 
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = () => false;
+}
+
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = () => {};
+}
+
+if (!Element.prototype.releasePointerCapture) {
+  Element.prototype.releasePointerCapture = () => {};
+}
+
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = () => {};
+}
+
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => {
   cleanup();
